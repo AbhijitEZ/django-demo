4ead591c3784d91db8c3abfb4b6fbc920ad68ae2
abhijitez <abhijit.nair659@gmail.com>
2019-09-02 17:44:39 +0530

    crud added

--- articles/urls.py
+++ articles/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index_view, add_view, detail_view
+from .views import index_view, add_view, detail_view, delete, edit_view
 urlpatterns = [
     path('', index_view, name='index'),
     path('add/', add_view, name='add'),
-    path('detail/<int:id>', detail_view, name='detail')
+    path('detail/<int:id>', detail_view, name='detail'),
+    path('delete/<int:id>', delete, name='delete'),
+    path('edit/<int:id>', edit_view, name='edit')
+
 
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
 from .models import Article
+from .forms import ArticleForm
 # Create your views here.
 
 
@@ -9,9 +11,30 @@ def index_view(request):
 
 
 def add_view(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
     return render(request, 'add.html', {})
 
 
 def detail_view(request, id):
     article = Article.objects.get(id=id)
     return render(request, 'detail.html', {"article": article})
+
+
+def delete(request, id):
+    article = Article.objects.get(id=id)
+    article.delete()
+    return redirect('index')
+
+
+def edit_view(request, id):
+    article = Article.objects.get(id=id)
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    return render(request, 'edit.html', {"article": article})
